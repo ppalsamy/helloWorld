f190231a2db5c8ce6455b96a71b68f7b786b552f
ppalsamy <pathmanaban.p@gmail.com>
2016-11-15 12:04:15 -0500

    added user greetings

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 	@RequestMapping(value="/greeting", method = RequestMethod.GET)
 	public String getGreeting(){
+		LOGGER.info("inside getGreetings()  ");
 		return "Hello World!";
+	}
+	
+	@RequestMapping(value="/greeting/{user}", method = RequestMethod.GET)
+	public String getUserGreeting(@PathVariable String user){
+		LOGGER.info("inside getUserGreetings()  ");
+		return String.format("Hello %1s!",user);
 	}
 }
